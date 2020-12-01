a9bc06ed11645f3a021cee5d3fa1f5ba8e7d41f1
Taylor Manderson <taylor.manderson@cubiko.com.au>
2020-12-01 15:48:41 +1000

    Day 1

--- src/Day1B.hs
+++ src/Day1B.hs
@@ -4,6 +4,6 @@ solve :: [Int] -> Maybe Int
 solve vals = case xs of
   [] -> Nothing
   (x:_) -> Just x
-  where xs = [a * b | a <- vals, b <- vals, (a + b) == 2020]
+  where xs = [a * b * c| a <- vals, b <- vals, c <- vals, (a + b + c) == 2020]
 
 main = getContents >>= (print . solve . fmap read . lines)
