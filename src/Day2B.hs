ca6c68f6f3f5ddaa1f8b7717f059c361c06821f6
Taylor Manderson <taylor.manderson@cubiko.com.au>
2020-12-02 15:28:42 +1000

    Day 2

---
+++ src/Day2B.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+import Data.String (fromString)
+import Data.Attoparsec.Text
+import qualified Data.Text as T
+import Data.Either (rights)
+
+data Line = Line {minCount :: Int, maxCount :: Int, letter :: Char, passsword :: String}
+
+parser :: Parser Line
+parser = do
+  mn <- read <$> many1 digit
+  char '-'
+  mx <- read <$> many1 digit
+  char ' '
+  l <- anyChar
+  string ": "
+  p <- many1 anyChar
+  pure $ Line mn mx l p
+
+isValid (Line mn mx l p) = ((p !! (mn - 1)) == l) /= ((p !! (mx - 1)) == l)
+
+solve :: [String] -> Int
+solve =  length . filter isValid . rights . fmap (parseOnly parser . fromString)
+
+main = getContents >>= (print . solve . fmap fromString . lines)
