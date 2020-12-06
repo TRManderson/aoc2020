58f5925dbf780a129e42dd4bd545649e39a60557
Taylor Manderson <taylor.manderson@cubiko.com.au>
2020-12-06 11:26:20 +1000

    Just use bools for day 5

--- src/Day5B.hs
+++ src/Day5B.hs
@@ -5,29 +5,19 @@ import Data.Functor ( ($>) )
 import Data.String (fromString)
 import Data.Either (rights)
 import Data.List (sort)
-import Numeric (showIntAtBase)
-import Data.Char (intToDigit)
 
-data Vertical = VFront | VBack
-  deriving (Eq, Show, Ord)
--- bc Either
-data Horizontal = HLeft | HRight
-  deriving (Eq, Show, Ord)
-
-hor = choice [ char 'L' $> HLeft
-             , char 'R' $> HRight
+hor = choice [ char 'L' $> False
+             , char 'R' $> True
              ]
-ver = choice [ char 'F' $> VFront
-             , char 'B' $> VBack
+ver = choice [ char 'F' $> False
+             , char 'B' $> True
              ]
 
-seat = (,) <$> count 7 ver <*> count 3 hor
+seat = (++) <$> count 7 ver <*> count 3 hor
 
-toValue :: ([Vertical], [Horizontal]) -> Int
-toValue (verts, hors) = (go vertsBool 0 64)*8 + (go horsBool 0 4)
+toValue :: [Bool] -> Int
+toValue vals = go vals 0 512
   where
-    vertsBool = (== VBack) <$> verts
-    horsBool = (== HRight) <$> hors
     go :: [Bool] -> Int -> Int -> Int
     go [] n _ = n
     go (x:xs) n w =
